bfe4e1ba509fb6a603c5ef34d2c85e82b1c56226
Mikhail Kashinin <flyaway400@gmail.com>
2021-09-18 22:41:01 +0800

    add tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  before_action :find_project
+
+  def index
+    @tasks = @project.tasks
+  end
+
+  def show
+    @task = @project.tasks.find_by(params[:id])
+  end
+
+  def new
+    @task = @project.tasks.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @task = @project.tasks.build(task_params)
+    if @task.save
+      flash[]
+      redirect_to user_project_path(current_user, @project), flash: { success: 'Task was created! '}
+    else
+      render :new, flash: { alert: 'Some errors occured'}
+    end
+  end
+
+  private
+
+  def find_project
+    @project = current_user.projects(params[:project_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:task_name, :body, :status, :deadline, :priority)
+  end
+
+end
