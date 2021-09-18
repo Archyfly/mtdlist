29425e59a9857e7372f572d2dfae5b0041e746dd
Mikhail Kashinin <flyaway400@gmail.com>
2021-09-18 12:22:49 +0800

    add enum status to model task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  enum status: [:created, :in_progress, :done]
+
 end
--- db/migrate/20210918040851_create_tasks.rb
+++ db/migrate/20210918040851_create_tasks.rb
@@ -1,8 +1,14 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
+      t.string :task_name
+      t.text :body
+      t.integer :status, default: :created
+      t.date :deadline
 
       t.timestamps
+
     end
+
   end
 end
