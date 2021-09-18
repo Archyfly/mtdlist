ff1d92c211c7751cbc90f0ecf4f49ffd4a7df31f
Mikhail Kashinin <flyaway400@gmail.com>
2021-09-19 02:10:42 +0800

    add toggle for projectDesc

---
+++ app/assets/javascript/tasks_show.js
@@ -0,0 +1,6 @@
+document.addEventListener('turbolinks:load', function() {
+  let controlOne = document.querySelector('#project-desc')
+  console.log(contolOne)
+  //if (controlOne) {controlOne.addEventListener('keyup', checkField, 'useCapture') }
+
+})
